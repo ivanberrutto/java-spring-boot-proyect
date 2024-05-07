a65c4ed2b52a21207b68d14a6fba183de537b8ec
Ivan Berrutto <ivanberrutto@gmail.com>
2024-05-06 23:00:12 -0300

    make UserHttpClient

--- src/main/java/dev/ivanberrutto/runnerz/user/UserRestClient.java
+++ src/main/java/dev/ivanberrutto/runnerz/user/UserRestClient.java
@@ -4,21 +4,30 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+
 @Component
-public class UserRestClient {
+class UserRestClient {
+
     private final RestClient restClient;
 
     public UserRestClient(RestClient.Builder builder) {
         this.restClient = builder
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .build();
-
     }
 
-    public User findAll(String id) {
+    List<User> findAll() {
         return restClient.get()
                 .uri("/users")
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {});
+    }
+
+    User findById(Integer id) {
+        return restClient.get()
+                .uri("/users/{id}", id)
+                .retrieve()
+                .body(User.class);
     }
 }
